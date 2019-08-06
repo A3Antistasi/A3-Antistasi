2d65bb4566ec327a13ae5129f3830de7c3b7d5a8
Tad <tdaykin@live.co.uk>
2019-08-06 23:03:12 +0100

    Cleanup repo

--- Tools/sqfvalidator/sqf/base_tokenizer.py
+++
@@ -1,7 +0,0 @@
-import re
-
-
-def tokenize(statement):
-    # the len=2 tokens have to be first!
-    regex = r'(\\\n|\r\n|>>|\/\*|\*\/|\|\||//|!=|<=|>=|==|\n|\t|[\"\' =:\{\}\(\)\[\];/,\!\/\*\%\^\-\+<>])'
-    return list(filter(None, re.split(regex, statement)))

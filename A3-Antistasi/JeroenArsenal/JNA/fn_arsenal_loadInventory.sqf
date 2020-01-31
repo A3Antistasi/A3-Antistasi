A3Antistasi/A3-Antistasi

commit 95fad1f01e7629c2876accdeb4f05efe918909b4

Fix several arsenal bugs(#726)
1. Fixed a bug where the arsenal used 24 rather than Antistasi's minWeaps param as the guest limit.
2. Fixed a bug where weapon accessories bypassed the guest limit.
3. Fixed some potential bugs with negative bullet counts and off-by-ones.
4. Fixed a bug where grenades wouldn't be throwable after loading a loadout.
5. Fixed a bug where the magazine guest limit was counting in mags rather than bullets.

4 is arguably an Arma bug. `(vestContainer player) addItemCargo "HandGrenade"` demonstrates the problem, at least in vanilla

A3-Antistasi/JeroenArsenal/JNA/fn_arsenal_loadInventory.sqf
@@ -154,16 +154,23 @@ removebackpack player;
 _isMember = [player] call A3A_fnc_isMember;
 _availableItems = [jna_dataList, _arrayPlaced] call _addArrays;
 _itemCounts =+ _availableItems;
+// reduce available items by guest limits for non-members
 {
 	_index = _foreachindex;
 	_subArray = _x;
+	_isMagArray = (_index == IDC_RSCDISPLAYARSENAL_TAB_CARGOMAG) || (_index == IDC_RSCDISPLAYARSENAL_TAB_CARGOMAGALL);
 	{
 		_item = _x select 0;
 		_amount = (_x select 1);
-		if (_amount != -1) then {
-			_amount = [(_x select 1) - (jna_minItemMember select _index),(_x select 1)] select _isMember;
+		if (_amount != -1 && !_isMember) then {
+			if !(_isMagArray) then { _amount = _amount - minWeaps }
+			else {
+				// Magazines are counted in bullets
+				_ammoCount = getNumber (configfile >> "CfgMagazines" >> _item >> "count");
+				_amount = _amount - memberOnlyMagLimit * _ammoCount;
+			};
+			_subArray set [_foreachindex, [_item,_amount]];
 		};
-		_subArray set [_foreachindex, [_item,_amount]];
 	} forEach _subArray;
 	_availableItems set [_index, _subArray];
 } forEach _availableItems;
@@ -238,7 +245,7 @@ _weapons = [_inventory select 6,_inventory select 7,_inventory select 8];
 			if (_amountMagAvailable > 0) then {
 				if (_amountMagAvailable < _amountMag) then {
 					_arrayMissing = [_arrayMissing,[_itemMag,_amountMag]] call jn_fnc_arsenal_addToArray;
-					_amountMag = _amountMagAvailable;
+					_amountMag = _amountMagAvailable max 0;
 				};
 			[_arrayTaken,_indexMag,_itemMag,_amountMag] call _addToArray;
 			[_availableItems,_indexMag,_itemMag,_amountMag] call _removeFromArray;
@@ -283,7 +290,7 @@ _weapons = [_inventory select 6,_inventory select 7,_inventory select 8];
 						};
 					};
 
-					if ((_indexAcc != -1) AND {[_availableItems select _indexAcc, _itemAcc] call jn_fnc_arsenal_itemCount != 0}) then {
+					if ((_indexAcc != -1) AND {[_availableItems select _indexAcc, _itemAcc] call jn_fnc_arsenal_itemCount > 0}) then {
 						switch _index do{
 							case IDC_RSCDISPLAYARSENAL_TAB_PRIMARYWEAPON:{player addPrimaryWeaponItem _itemAcc;};
 							case IDC_RSCDISPLAYARSENAL_TAB_SECONDARYWEAPON:{player addSecondaryWeaponItem _itemAcc;};
@@ -353,6 +360,16 @@ private _addContainerFuncs = [
 	};
 } forEach _containers;
 
+// because addItemCargo doesn't enable grenades
+_addItemToContainer = {
+	params ["_containerIndex", "_item"];
+	switch (_containerIndex) do {
+		case 0: { player addItemToUniform _item };
+		case 1: { player addItemToVest _item };
+		default { player addItemToBackpack _item };
+	};
+};
+
 //add items to containers
 {
 	_container = call (_x select 0);
@@ -374,24 +391,22 @@ private _addContainerFuncs = [
 					};
 
 					if(_amountAvailable < _amount) then {
-						_amount = _amountAvailable;
 						_arrayMissing = [_arrayMissing,[_item,(_amount - _amountAvailable)]] call jn_fnc_arsenal_addToArray;
+						_amount = _amountAvailable max 0;
 					};
 					[_arrayTaken,_index,_item,_amount] call _addToArray;
 					[_availableItems,_index,_item,_amount] call _removeFromArray;
-					if (_amount>0) then {//prevent empty mags
-						_container addMagazineAmmoCargo  [_item,1, _amount];
-					};
+					_container addMagazineAmmoCargo  [_item,1, _amount];
 				};
 			} else {
 				_amount = 1;
 				call {
 					if ([_itemCounts select _index, _item] call jn_fnc_arsenal_itemCount == -1) exitWith {
-						_container addItemCargo [_item, 1];
+						[_forEachIndex, _item] call _addItemToContainer;
 					};
 
-					if (_amountAvailable > _amount) then {
-						_container addItemCargo [_item,_amount];
+					if (_amountAvailable >= _amount) then {
+						[_forEachIndex, _item] call _addItemToContainer;
 						[_arrayTaken,_index,_item,_amount] call _addToArray;
 						[_availableItems,_index,_item,_amount] call _removeFromArray;
 					} else {

A3-Antistasi/JeroenArsenal/JNA/fn_arsenal_init.sqf
@@ -43,9 +43,11 @@ IDC_RSCDISPLAYARSENAL_TAB_CARGOPUT		23
 IDC_RSCDISPLAYARSENAL_TAB_CARGOMISC		24
 IDC_RSCDISPLAYARSENAL_TAB_CARGOMAGALL		26
 */
-//jna_minItemMember = [-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1];
-jna_minItemMember = [24,24,24,24,24,24,24,24,24,24,24,24,24,24,24,24,24,24,24,24,24,memberOnlyMagLimit,24,24,24,24,memberOnlyMagLimit];
-
+jna_minItemMember = [-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1];
+//jna_minItemMember = [24,24,24,24,24,24,24,24,24,24,24,24,24,24,24,24,24,24,24,24,24,memberOnlyMagLimit,24,24,24,24,memberOnlyMagLimit];
+jna_minItemMember = jna_minItemMember apply { minWeaps };
+jna_minItemMember set [IDC_RSCDISPLAYARSENAL_TAB_CARGOMAG, memberOnlyMagLimit];
+jna_minItemMember set [IDC_RSCDISPLAYARSENAL_TAB_CARGOMAGALL, memberOnlyMagLimit];
 //preload the ammobox so you dont need to wait the first time
 ["Preload"] call jn_fnc_arsenal;
 

commit 4eab3d4298c93642d5182dcb96b06edf48e85b74

Fix loadout container scrambling (#749)
Fixed a bug introduced in #726 which caused some items to placed in the wrong uniform/vest/backpack container when loading a saved loadout.

A3-Antistasi/JeroenArsenal/JNA/fn_arsenal_loadInventory.sqf
@@ -374,6 +374,7 @@ _addItemToContainer = {
 {
 	_container = call (_x select 0);
 	_items = _x select 1;
+	_containerIndex = _forEachIndex;
 
 	{
 		_item = _x;
@@ -402,11 +403,11 @@ _addItemToContainer = {
 				_amount = 1;
 				call {
 					if ([_itemCounts select _index, _item] call jn_fnc_arsenal_itemCount == -1) exitWith {
-						[_forEachIndex, _item] call _addItemToContainer;
+						[_containerIndex, _item] call _addItemToContainer;
 					};
 
 					if (_amountAvailable >= _amount) then {
-						[_forEachIndex, _item] call _addItemToContainer;
+						[_containerIndex, _item] call _addItemToContainer;
 						[_arrayTaken,_index,_item,_amount] call _addToArray;
 						[_availableItems,_index,_item,_amount] call _removeFromArray;
 					} else {
baf546e43ead6d3d409a2820135b39437dd64bf8
Spoffy <contact@spoffy.net>
2020-02-07 15:46:58 +0000

    Adds A3A_fnc_generateRoadsDB
    Usage: copyToClipboard str (call A3A_fnc_generateRoadsDB)

--- A3-Antistasi/functions.hpp
+++ A3-Antistasi/functions.hpp
@@ -484,6 +484,7 @@ class A3A
 		class basicBackpack {};
 		class createDataObject {};
 		class dateToTimeString {};
+		class generateRoadsDB {};
 		class log {};
 		class vehicleWillCollideAtPosition {};
 	};

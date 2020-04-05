d9d8554d60c1f93870bf9bbae18a74e1e3c55c9c
czarny <czarny@czarny.org>
2020-04-05 15:46:54 +0200

    Mission generation scripts generate smaller mission PBO.
    # Conflicts:
    #	A3-Antistasi/Stringtable.xml
    #	deployCzarny.ps1

--- PrepareMissionForPacking.ps1
+++ PrepareMissionForPacking.ps1
@@ -17,9 +17,11 @@ $formattedVersionId = $versionId.Split("\.") -join "-";
 
 ForEach ($templateFolder in $missionTemplateFolders) {
 	$folderName = $templateFolder.Name;
-	$pair = $folderName.Split("\.");
+    $pair = $folderName.Split("\.");
 	$missionFolderName = $pair[0] + "-" + $formattedVersionId + "." + $pair[1]; 
 	$destinationPath = $(Join-Path $folderForPreparedMissions.FullName $missionFolderName);
 	Copy-Item -Path $mainDataPath -Destination $destinationPath -Recurse;
+    $exclude = "NavGrid"+ $pair[1] + ".sqf";
+    Get-ChildItem $destinationPath\NavGrids -Exclude $exclude | Remove-Item -Force;
 	Copy-Item -Path $(Join-Path $templateFolder.FullName "*") -Destination $destinationPath -Recurse -Force;
 }

742f232c94b9d3509ecb6ce8f642e9bcb45fc051
Caleb Serafin <calebserafin@outlook.com>
2021-01-13 15:05:36 +0200

    Common Description.ext contents moved to MissionDescription (#1637)
    ## What type of PR is this.
    * Bug
    * Change
    3. [x] Enhancement
    
    ### What have you changed and why?
    * Common Description.ext contents moved files to MissionDescription.
    * Due to instances where a map template has some custom scripts, CfgXX class is still defined there and includess contents from MissionDescription\CfgXXXContents.
    * Idea: 0060 Move the All the non specific information from description.ext into the MissionDescription Folder, would make adding sub functions and defines easier.
    * 0060 - Yes will be done. Everything besides the strings, as well as the picture references and class CfgDebriefing will be put in one file so we only have one include in the description.ext
    
    ### Please specify which Issue this PR Resolves.
    closes https://github.com/official-antistasi-community/A3-Antistasi/issues/1619
    
    ### Please verify the following and ensure all checks are completed.
    
    2. [x] Have you loaded the mission in LAN host?
    3. [ ] Have you loaded the mission on a dedicated server?
    
    ### Is further testing or are further changes required?
    1. [x] No
    * Yes
    
    NB: All `description.ext`s were changed by mass find and replace. All of them except `Antistasi-ChernarusWinter.chernarus_winter` are formatted exactly same.
    Test Altis, Statis and ChernarusWinter.
    All missions in the test were Built with ADD.
    
    ### How can the changes be tested?
    Steps: 
    Build and load missions.
    
    * Added master.hpp & others for mission src includes
    
    * All Map-Templates conceed to changes.

---
+++ A3-Antistasi/MissionDescription/CfgSoundsContents.hpp
@@ -0,0 +1,8 @@
+// In map template description.ext use:
+// #include "MissionDescription\CfgSoundsContents.hpp"
+class fire
+{
+	name="fire";
+	sound[]={"Music\fire.ogg",db+12,1.0}; // NB, this will resolve from root
+	titles[]={};
+};
---
+++ A3-Antistasi/MissionDescription/master.hpp
@@ -0,0 +1,14 @@
+// In map template description.ext use:
+// #include "MissionDescription\master.hpp"
+// Whether order should be maintained is unknown.
+#include "..\defines.hpp"
+#include "..\dialogs.hpp"
+
+author = $STR_antistasi_credits_generic_author_text;
+loadScreen = "Pictures\Mission\pic.jpg"; // NB, this will resolve from root
+overviewPicture = "Pictures\Mission\pic.jpg"; // NB, this will resolve from root
+
+#include "debug.hpp"
+#include "gameSettings.hpp"
+#include "params.hpp"
+#include "CfgIdentities.hpp"

c22d88c87e65df983359d5a43bae0651a8d7210d
Bob-Murphy <50651578+Bob-Murphy@users.noreply.github.com>
2021-02-13 02:13:49 +0100

    typo fix in master.hpp

--- A3-Antistasi/MissionDescription/master.hpp
+++ A3-Antistasi/MissionDescription/master.hpp
@@ -7,7 +7,7 @@
 author = $STR_antistasi_credits_generic_author_text;
 loadScreen = "Pictures\Mission\pic.jpg"; // NB, this will resolve from root
 overviewPicture = "Pictures\Mission\pic.jpg"; // NB, this will resolve from root
-Keys[] = {"A3-Antistasi-is-not-availible-in-single-player"};
+Keys[] = {"A3-Antistasi-is-not-available-in-single-player"};
 KeysLimit = 2;  // Even if player tampers with his unlocked keys, this will never become true.
 
 #include "debug.hpp"

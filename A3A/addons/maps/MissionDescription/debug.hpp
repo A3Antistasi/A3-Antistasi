4adc747ca888197b47c14f33e8b693ac075de6c8
Bob-Murphy <50651578+Bob-Murphy@users.noreply.github.com>
2022-01-30 22:51:36 +0100

    Update debug.hpp

--- A3A/addons/maps/MissionDescription/debug.hpp
+++ A3A/addons/maps/MissionDescription/debug.hpp
@@ -2,17 +2,12 @@ allowFunctionsLog = 1;
 enableDebugConsole[] = {
     "76561198977000729", //antistasi_official
     "76561197981991967", //Bob Murphy
-    "76561198075487693", //Meerkat
     "76561198011383725", //Spoffy
-    "76561198105754435", //Wurzel
     "76561197993612092", //DoomMetal
     "76561198304402577", //FrostsBite
     "76561198087513761", //Hakon
     "76561198000461190", //John Jordan
     "76561198016623887", //Lazejun
-    "76561197991835568", //Tad
-    "76561198068206504", //poweredbypot
     "76561197969814277", //Giddi
-    "76561198041429764"  //Killerswin2
 };
 allowFunctionsRecompile = 1;
